8403a9b384f0ed9d179aa1010ccd084a89c9c806
Victor Assis <victor.assis@hey.com>
2024-04-03 15:40:26 -0300

    create createInitialPopulation function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
@@ -17,13 +18,14 @@ struct Item
     Item(int id, int value, int weight) : id(id), value(value), weight(weight) {}
 };
 
+
 vector<Item> createRandomItems()
 {
     random_device rd;
     mt19937 gen(rd());
 
     uniform_int_distribution<> numberOfItemsDistr(1, MAX_NUMBER_OF_ITEMS);
-    int numberOfItems = numberOfItemsDistr(gen);
+    const int numberOfItems = numberOfItemsDistr(gen);
 
     int value;
     int weight;
@@ -44,14 +46,46 @@ vector<Item> createRandomItems()
     return items;
 }
 
+vector<vector<int>> createInitialPopulation(int populationSize)
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> solutionDistr(0, 1);
+
+    int numberSolutionsPerPopulation = 16;
+    vector<vector<int>> initialPopulation(populationSize, vector<int>(numberSolutionsPerPopulation));
+
+    for (int i = 0; i < populationSize; ++i)
+    {
+        for (int j = 0; j < numberSolutionsPerPopulation; ++j)
+        {
+            initialPopulation[i][j] = solutionDistr(gen);
+        }
+    }
+
+    return initialPopulation;
+}
+
+
 int main()
 {
     vector<Item> items = createRandomItems();
 
     for (Item& item : items)
     {
-        cout << item.id << endl;
+        cout << "ID: " << item.id << " VALUE: " << item.value << " WEIGHT: " << item.weight << endl;
     }
+
+    const int knapsackCapacity = 100;
+
+    int populationSize = items.size();
+
+    auto initialPopulation = createInitialPopulation(populationSize);
+
+    int numberOfGenerations = 1000;
+
+    
+
     
     return 0;
 }
